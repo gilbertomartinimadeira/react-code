da79bd9a1a37a1933a358d6032cdd5f5b65c47b5
Gilberto Martini Madeira <gilberto.madeira@outlook.com>
2024-03-11 19:28:00 -0300

    Cleaned app

--- website/my-react-app/src/App.jsx
+++ website/my-react-app/src/App.jsx
@@ -1,22 +1,21 @@
-import List from "./List";
 
 function App() {
 
-  const fruits = [
-    {id:1,name:'apple', calories:95}, 
-    {id:2,name:'banana',calories:105}, 
-    {id:3,name:'orange',calories:45}, 
-    {id:4,name:'coconut',calories:159}, 
-    {id:5,name:'pineapple',calories:37}
-];
+//   const fruits = [
+//     {id:1,name:'apple', calories:95}, 
+//     {id:2,name:'banana',calories:105}, 
+//     {id:3,name:'orange',calories:45}, 
+//     {id:4,name:'coconut',calories:159}, 
+//     {id:5,name:'pineapple',calories:37}
+// ];
 
-const vegetables = [
-  {id:6,name:'potato', calories:110}, 
-  {id:7,name:'celery',calories:14}, 
-  {id:8,name:'carrot',calories:25}, 
-  {id:9,name:'corn',calories:63}, 
-  {id:10,name:'broccoli',calories:50}
-];
+// const vegetables = [
+//   {id:6,name:'potato', calories:110}, 
+//   {id:7,name:'celery',calories:14}, 
+//   {id:8,name:'carrot',calories:25}, 
+//   {id:9,name:'corn',calories:63}, 
+//   {id:10,name:'broccoli',calories:50}
+// ];
   return(
     <>   
       {/* <Student name="Gilberto" age={40} isStudent={true}/>
@@ -27,8 +26,8 @@ const vegetables = [
 
       {/* <UserGreeting isLoggedIn={false} username="Gilberto"/>
       <UserGreeting isLoggedIn={true}/> */}
-      {fruits.length > 0 && <List items={fruits} category="Fruits"/> }
-      {vegetables.length > 0 && <List items={vegetables} category="Vegetables"/> }
+      {/* {fruits.length > 0 && <List items={fruits} category="Fruits"/> }
+      {vegetables.length > 0 && <List items={vegetables} category="Vegetables"/> } */}
     </>
   );
 }
